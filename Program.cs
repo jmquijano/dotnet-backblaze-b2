f6493ed9837301190aeacfb18e930d7f082d9ea2
Justine <justinelouis.quijano@ama.edu.ph>
2023-09-18 05:12:33 +0800

    feat: Configure S3 SDK.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +11,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration
+    .AddJsonFile("appsettings.Backblaze.json", reloadOnChange: true, optional: false);
+
+// AWS S3
+AWSOptions awsOptions = new AWSOptions()
+{
+    Credentials = new BasicAWSCredentials(
+        builder.Configuration["Backblaze:KeyID"],
+        builder.Configuration["Backblaze:ApplicationKey"]
+    ),
+    Region = RegionEndpoint.USEast1,
+    DefaultClientConfig =
+    {
+        ServiceURL = builder.Configuration["Backblaze:ServiceUrl"]
+    }
+};
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonS3>();
 
 var app = builder.Build();
 
